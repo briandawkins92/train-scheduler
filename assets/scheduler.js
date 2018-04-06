b262de3d4bf32d7f1adc665dfa745e40ac98942e
Brian Dawkins <bkdawkins@gmail.com>
2018-04-06 12:13:45 -0700

    index.html and style.css complete

---
+++ assets/scheduler.js
@@ -0,0 +1,14 @@
+/* BEGIN JAVASCRIPT*/
+var config = {
+    apiKey: "AIzaSyCM5WiB8qnyPpqF-Tlfoz0SHO4kp7olbhw",
+    authDomain: "train-schedule-bkd.firebaseapp.com",
+    databaseURL: "https://train-schedule-bkd.firebaseio.com",
+    projectId: "train-schedule-bkd",
+    storageBucket: "train-schedule-bkd.appspot.com",
+    messagingSenderId: "227969918749"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+/* END JAVASCRIPT*/
